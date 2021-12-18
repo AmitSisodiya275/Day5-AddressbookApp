8a629efb4123137b6e9c44e8e0fee851d2ca3478
AmitSisodiya275 <amitsisodiya275@gmail.com>
2021-12-18 07:58:36 +0530

    Added: a controller which can handle various http request.

---
+++ src/main/java/com/bridgelab/addressbook/controller/Controller.java
@@ -0,0 +1,44 @@
+package com.bridgelab.addressbook.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import ch.qos.logback.core.pattern.util.RegularEscapeUtil;
+
+@RestController
+@RequestMapping("/addressbook")
+public class Controller {
+
+	@GetMapping("/get")
+	public ResponseEntity<String> getAllUser() {
+		return new ResponseEntity<String>("Get call for all user successful.", HttpStatus.OK);
+	}
+
+	@GetMapping("/get/{eId}")
+	public ResponseEntity<String> geUserById(@PathVariable("eId") int eId) {
+		return new ResponseEntity<String>("Get call for specific id is successful id : " + eId, HttpStatus.OK);
+	}
+
+	@PostMapping("/add")
+	public ResponseEntity<String> addUser() {
+		return new ResponseEntity<String>("Post call successful ", HttpStatus.OK);
+	}
+
+	@PutMapping("/update/{id}")
+	public ResponseEntity<String> updateUser(@PathVariable("id") int id) {
+		return new ResponseEntity<String>("data updated for id : " + id, HttpStatus.OK);
+	}
+
+	@DeleteMapping("/delete/{id}")
+	public ResponseEntity<String> deleteUser(@PathVariable("id") int id) {
+		return new ResponseEntity<String>("Data deleted for id : " + id, HttpStatus.OK);
+	}
+}
